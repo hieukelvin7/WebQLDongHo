9ebd57e9721f0c6f7212c129060df5cabc95eea8
hieukelvin7 <hieukelvin7@gmail.com>
2021-06-25 21:19:57 +0700

    Initial commit

---
+++ WebDongHo/Controllers/HomeUserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDongHo.Controllers
+{
+    public class HomeUserController : Controller
+    {
+        // GET: HomeUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ WebDongHo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebDongHo.MvcApplication" Language="C#" %>
