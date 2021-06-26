bced06b19ef17516455921365ccb7a070e22966f
hieukelvin7 <hieukelvin7@gmail.com>
2021-06-26 09:49:23 +0700

    up

--- WebDongHo/Controllers/HomeUserController.cs
+++ WebDongHo/Controllers/HomeUserController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebDongHo.Models;
 namespace WebDongHo.Controllers
 {
     public class HomeUserController : Controller
     {
+        DBDongHoDataContext data = new DBDongHoDataContext();
         // GET: HomeUser
+        
+        public List<SanPham> SanPhamMoi(int count)
+        {
+            return data.SanPhams.OrderByDescending(a => a.NgayCapNhap).Take(count).ToList();
+        }
         public ActionResult Index()
         {
+            var spmoi = SanPhamMoi(4);
+            return View(spmoi);
+        }
+        public ActionResult DanhMucSanPham()
+        {
+            var danhmuc = from dm in data.DanhMucSanPhams select dm;
             return View();
         }
-        
+        public ActionResult Detail(int id)
+        {
+            var sp = from s in data.SanPhams
+                     where s.MaSanPham == id
+                     select s;
+            return View(sp.Single());
+        }
     }
 }
