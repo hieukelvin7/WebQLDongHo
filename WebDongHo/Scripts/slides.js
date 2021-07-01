ba26df1d0c6cc42d85d8f5c65c97428f3ae538ff
tuilalong <longnguyen2431@gmail.com>
2021-07-02 00:54:59 +0700

    upcode lần 5

--- WebDongHo/Scripts/slides.js
+++ WebDongHo/Scripts/slides.js
@@ -8,3 +8,43 @@ setInterval(function(){
         count = 1;
     }
 }, 4000)
+
+
+// Sliders details
+
+var imgs = document.getElementsByClassName('imgs');
+
+var activeImg = document.getElementsByClassName('active');
+
+for (var i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener('mouseover', function () {
+
+        if (activeImg.length > 0) {
+            activeImg[0].classList.remove('active')
+        }
+
+        this.classList.add('active')
+        document.getElementById('img__big').src = this.src
+    });
+}
+
+
+// Tabs detail
+
+var $ = document.querySelector.bind(document)
+var $$ = document.querySelectorAll.bind(document)
+
+var tabs = $$('.info-item')
+var panes = $$('.tab-pane')
+
+tabs.forEach((tab, index) => {
+    var pane = panes[index]
+
+    tab.onclick = function () {
+        $('.info-item.active').classList.remove('active')
+        $('.tab-pane.active').classList.remove('active')
+
+        this.classList.add('active')
+        pane.classList.add('active')
+    }
+});
